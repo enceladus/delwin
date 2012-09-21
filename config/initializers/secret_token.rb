f7d09cea642c99f2eec7ac12362799c3550818a2
Delwin Campbell <d@delw.in>
2012-09-21 18:27:35 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Delwin::Application.config.secret_token = '69681fbafc1c263119bf47889646d184e1586e67f0fc1dd43739a66f80cdb0449d39712ca8d920107ef4f16b9f71dd298a9cb34d1384e38b5f3727fe97d937be'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Delwin::Application.config.session_store :cookie_store, key: '_delwin_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Delwin::Application.config.session_store :active_record_store
