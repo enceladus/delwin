fb1c3eb8b0ba190be1cfd542fd9573927949b76c
Delwin Campbell <d@delw.in>
2012-10-01 19:45:32 -0500

    added uploader to books model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ActiveRecord::Base
   attr_accessible :author, :cover_image, :date_finished, :date_started, :description, :link, :summary, :title
+  
+  mount_uploader :cover_image, ImageUploader
 end
